37d7fc7b6c02aaa884b38ba05e25bcdbf7c21009
selvin95 <s.eliezer.perez@gmail.com>
2023-03-21 00:06:44 -0600

    nueva actualizacion

--- Seguridad/Security.js
+++ Seguridad/Security.js
@@ -1,50 +1,51 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-async function validaSeguridad(token){
-    try {
-        let retorno
-        if (!token) {
-            retorno =  'N'
-        }else{
-           await jwt.verify(token, 'secretDEV2023', function(err, decoded) {
-                if (err) {
-                    retorno =  'N'
-                } else {
-                    // const now = Math.floor(Date.now() / 1000); // Timestamp UNIX actual
-                    // if (now - decoded.iat > decoded.expiresIn) {
-                    //     retorno =  'T'
-                    //   } else {
-                        if(decoded.id !== 'devCofee2023'){
-                            retorno = 'N'
-                        }else{
-                            retorno = decoded.usuario
-                        }
-                    //   }
-                }
-            })
-            return retorno
+async function validaSeguridad(token) {
+  try {
+    let retorno;
+    if (!token) {
+      retorno = "N";
+    } else {
+      await jwt.verify(token, "secret", function (err, decoded) {
+        if (err) {
+          console.log("probando..." + err);
+          retorno = "N";
+        } else {
+          const now = Math.floor(Date.now() / 1000); // Timestamp UNIX actual
+          if (now - decoded.iat > decoded.expiresIn) {
+            retorno = "T";
+          } else {
+            if (!decoded.id_login) {
+              retorno = "N";
+            }
+            retorno = decoded.id_login.toString();
+          }
+          //   }
         }
-    } catch (error) {
-        console.log(error)
-          return 1
-      }
-  }
-
-  async function creaToken(usuario, id_login){
-    try {
-        const payload = {
-            usuario: usuario,
-            id_login: id_login,
-            expiresIn: 3600//00
-        }
-        const token = jwt.sign(payload, 'secret');
-        return token
-    } catch (error) {
-        console.log("error en crea token")
+      });
+      return retorno;
     }
+  } catch (error) {
+    console.log(error);
+    return 1;
   }
+}
 
-  module.exports ={
-    validaSeguridad : validaSeguridad,
-    creaToken       :  creaToken
+async function creaToken(usuario, id_login) {
+  try {
+    const payload = {
+      usuario: usuario,
+      id_login: id_login,
+      expiresIn: 3600, //00
+    };
+    const token = jwt.sign(payload, "secret");
+    return token;
+  } catch (error) {
+    console.log("error en crea token");
   }
+}
+
+module.exports = {
+  validaSeguridad: validaSeguridad,
+  creaToken: creaToken,
+};
